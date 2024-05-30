7f372c52b96a671c866a96deebe6bffbeb01e24d
curious_andrew <afirsov410@gmail.com>
2024-05-30 14:37:47 +0300

    correct example

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import "dotenv/config";
 import { upsertDocuments } from "./src/upserters/upsertDoc.js";
 import { fileURLToPath } from "url";
 import path from "path";
+import { upsertSitemap } from "./src/upserters/upsertSitemap";
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(path.dirname(__filename));
 
-await upsertDocuments("docs/fromGmp");
-await upsertDocuments("docs/fromTg");
+await upsertDocuments("docs");
+await upsertSitemap("example.com/sitemap.xml", "article");
